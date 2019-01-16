476ec3f8589241f12e431de8a54d8abdb33f059c
JESSICA A BETTS <jessicaannbettsis@gmail.com>
2019-01-16 04:07:28 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,37 @@
-# Write your code here.
+def word_substituter(tweet)
+  tweet_array = tweet.split(" ")
+  i = 0
+  while i < tweet_array.length
+    word = tweet_array[i]
+    short_word = dictionary(word)
+    #puts "original: #{word}, new: #{short_word}"
+    tweet_array[i] = short_word
+    i += 1
+  end
+  return tweet_array.join(" ")
+end
+
+def dictionary(word)
+  
+  dictionary_hash = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+  
+  if dictionary_hash.keys.include? word
+    return dictionary_hash.fetch(word)
+  end
+  return word
+end
+
+def bulk_tweet_shortener(tweets)
+  puts tweets.length
+end
