5a2c2d00aefaab6d1493615554ae81bdfda106a7
JESSICA A BETTS <jessicaannbettsis@gmail.com>
2019-01-16 04:18:35 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,52 @@
-# Write your code here.
+def word_substituter(tweet)
+  tweet_array = tweet.split(" ")
+  i = 0
+  while i < tweet_array.length
+    word = tweet_array[i]
+    short_word = dictionary(word)
+    #puts "original: #{word}, new: #{short_word}"
+    tweet_array[i] = short_word
+    i += 1
+  end
+  return tweet_array.join(" ")
+end
+
+def dictionary(word)
+  dictionary_hash = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+  
+  if dictionary_hash.keys.include? word.downcase
+    return dictionary_hash.fetch(word.downcase)
+  end
+  return word
+end
+
+def bulk_tweet_shortener(tweets)
+  tweets.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    tweet = word_substituter(tweet)
+  end
+  return tweet
+end
+
+def shortened_tweet_truncator(tweet)
+  if tweet.length > 140
+    tweet = tweet[0..139]
+  end
+  return tweet
+end
